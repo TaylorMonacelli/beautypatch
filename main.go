cebb8c1198abb0240c419bf766f71fe3987ff00c
Taylor Monacelli <taylormonacelli@gmail.com>
2023-04-27 09:57:56 -0700

    real men dont make git commit comments

--- main.go
+++ main.go
@@ -3,52 +3,56 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	filePaths := []string{
+	paths := []string{
 		"C:\\Windows\\System32\\control.exe",
 		"C:\\Windows\\System32\\windowspowershell\\v1.0\\powershell.exe",
 		"C:\\Windows\\explorer.exe",
 	}
 
-	xmlTemplate := `<?xml version="1.0" encoding="utf-8"?>
-<LayoutModificationTemplate
-    xmlns="http://schemas.microsoft.com/Start/2014/LayoutModification"
-    xmlns:defaultlayout="http://schemas.microsoft.com/Start/2014/FullDefaultLayout"
-    xmlns:start="http://schemas.microsoft.com/Start/2014/StartLayout"
-    xmlns:taskbar="http://schemas.microsoft.com/Start/2014/TaskbarLayout"
-    Version="1">
-  <CustomTaskbarLayoutCollection PinListPlacement="Replace">
-    <defaultlayout:TaskbarLayout>
-      <taskbar:TaskbarPinList>
-      </taskbar:TaskbarPinList>
-    </defaultlayout:TaskbarLayout>
-  </CustomTaskbarLayoutCollection>
-</LayoutModificationTemplate>`
+	// create a new logger instance
+	logger := logrus.New()
 
-	for _, path := range filePaths {
+	// set logger output to stdout
+	logger.SetOutput(os.Stdout)
+
+	// create a new taskbar pin list
+	taskbarPinList := ""
+
+	// iterate over paths
+	for _, path := range paths {
+		// check if the file exists
 		if _, err := os.Stat(path); os.IsNotExist(err) {
-			log.WithFields(log.Fields{
-				"path": path,
-			}).Info("File does not exist")
-		} else {
-			log.WithFields(log.Fields{
-				"path": path,
-			}).Info("File exists")
-
-			xmlTemplate = fmt.Sprintf("%s\n<taskbar:DesktopApp DesktopApplicationLinkPath=\"%s\" />", xmlTemplate, path)
-
-			docStartIndex := len(xmlTemplate) - len("</taskbar:TaskbarPinList>\n      </defaultlayout:TaskbarLayout>\n    </CustomTaskbarLayoutCollection>\n  </LayoutModificationTemplate>")
-			xmlTemplate = xmlTemplate[:docStartIndex] + fmt.Sprintf("\n<taskbar:DesktopApp DesktopApplicationLinkPath=\"%s\" />", path) + xmlTemplate[docStartIndex:]
-
-			log.WithFields(log.Fields{
-				"path": path,
-			}).Info("Path added to XML template")
+			logger.Warnf("%s does not exist", path)
+			continue
 		}
+
+		// add the path to the taskbar pin list
+		taskbarPinList += fmt.Sprintf(`<taskbar:DesktopApp DesktopApplicationLinkPath="%s" />\n`, filepath.ToSlash(path))
+		logger.Infof("%s added to the taskbar pin list", path)
 	}
 
-	fmt.Println(xmlTemplate)
+	// create the layout modification template
+	layoutModificationTemplate := fmt.Sprintf(`<?xml version="1.0" encoding="utf-8"?>
+	<LayoutModificationTemplate xmlns="http://schemas.microsoft.com/Start/2014/LayoutModification"
+	    xmlns:defaultlayout="http://schemas.microsoft.com/Start/2014/FullDefaultLayout"
+	    xmlns:start="http://schemas.microsoft.com/Start/2014/StartLayout"
+	    xmlns:taskbar="http://schemas.microsoft.com/Start/2014/TaskbarLayout"
+	    Version="1">
+	  <CustomTaskbarLayoutCollection PinListPlacement="Replace">
+	    <defaultlayout:TaskbarLayout>
+	      <taskbar:TaskbarPinList>
+	        %s
+	      </taskbar:TaskbarPinList>
+	    </defaultlayout:TaskbarLayout>
+	  </CustomTaskbarLayoutCollection>
+	</LayoutModificationTemplate>`, taskbarPinList)
+
+	// log the layout modification template
+	logger.Infof("Layout modification template:\n%s", layoutModificationTemplate)
 }
